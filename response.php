b5da0dbccba97045579dfe2f7f460216ea039c14
ShaorenC <91179333+ShaorenCheah@users.noreply.github.com>
2024-06-07 22:17:31 +0800

    Added .env
    - Environment variables
    - Added phpdotenv dependency

--- response.php
+++ response.php
@@ -1,13 +1,19 @@
 <?php
 require "vendor/autoload.php";
+require __DIR__ . '/vendor/autoload.php';
 
+use Dotenv\Dotenv;
 use GeminiAPI\Client;
 use GeminiAPI\Resources\Parts\TextPart;
+
+// Load environment variables 
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 $data = json_decode(file_get_contents("php://input"));
 
 $text = $data->text;
-$apiKey = "AIzaSyDP5hBFoBfibXltol8IfiNxm5Yg0FDPggA";
+$apiKey = $_ENV['API_KEY'];
 $client = new Client($apiKey);
 
 $response =  $client->geminiPro()->generateContent(
